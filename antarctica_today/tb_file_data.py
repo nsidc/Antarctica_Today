d6425a6541ccce0460a545abeb731ccfa3d58038
Matt Fisher <matt.fisher@nsidc.org>
2023-09-28 17:37:06 -0600

    Remove DATA_VERSION conditionals
    DATA_VERSION is a constant set to 3, so only one conditional branch is
    used. To use older versions of the data, use older versions of the code.

--- antarctica_today/tb_file_data.py
+++ antarctica_today/tb_file_data.py
@@ -52,151 +52,51 @@ antarctic_regions_dict = {
 
 NSIDC_0080_file_dir = "../Tb/nsidc-0080"
 
-DATA_VERSION = 3
+model_results_v3_dir = "../data/"
+model_results_v3_picklefile = os.path.join(
+    model_results_v3_dir, "v3_1979-present_raw.pickle"
+)
+model_results_v3_picklefile_gap_filled = os.path.join(
+    model_results_v3_dir, "v3_1979-present_gap_filled.pickle"
+)
 
-if DATA_VERSION == 1:
-    # Temporarily cut these out to reduce overhead when importing.
-    # model_results_v1_file_list = recurse_files(model_results_v1_dir)
-    # model_results_v1_file_list = None
-    model_results_v1_dir = "../data/v1"
-
-    model_results_v1_picklefile = os.path.join(
-        model_results_v1_dir, "v1_2000-2020.pickle"
-    )
-    model_results_v1_plot_directory = "../plots/v1"
-    outputs_v1_tifs_directory = "../qgis/sample_results/v1"
-    outputs_v1_annual_tifs_directory = "../qgis/sample_results/v1_annual"
-    outputs_v1_annual_plots_directory = os.path.join(
-        model_results_v1_plot_directory, "Cumulative Annual Plots"
-    )
-
-    model_results_dir = model_results_v1_dir
-    model_results_picklefile = model_results_v1_picklefile
-    model_results_plot_directory = model_results_v1_plot_directory
-    output_tifs_directory = outputs_v1_tifs_directory
-    outputs_annual_tifs_directory = outputs_v1_annual_tifs_directory
-    outputs_annual_plots_directory = outputs_v1_annual_plots_directory
-elif DATA_VERSION == 2:
-    model_results_v2_dir = "../data/v2"
-
-    model_results_v2_plot_directory = "../plots/v2"
-    outputs_v2_tifs_directory = "../qgis/sample_results/v2"
-    outputs_v2_annual_tifs_directory = "../qgis/sample_results/v2_annual"
-    outputs_v2_annual_plots_directory = os.path.join(
-        model_results_v2_plot_directory, "Cumulative Annual Maps"
-    )
-    model_results_v2_picklefile = os.path.join(
-        model_results_v2_dir, "v2_1979-2020.pickle"
-    )
-
-    model_results_dir = model_results_v2_dir
-    model_results_picklefile = model_results_v2_picklefile
-    model_results_plot_directory = model_results_v2_plot_directory
-    output_tifs_directory = outputs_v2_tifs_directory
-    outputs_annual_tifs_directory = outputs_v2_annual_tifs_directory
-    outputs_annual_plots_directory = outputs_v2_annual_plots_directory
-elif DATA_VERSION == 2.5:
-    # For version 2.5 of the data, with coded filtering thresholds, use this as the default.
-    default_melt_code_threshold = 4
-    model_results_v2_5_dir = "../data/v2.5"
-
-    model_results_v2_5_picklefile = os.path.join(
-        model_results_v2_5_dir, "v2.5_1979-2021.pickle"
-    )
-    model_results_v2_5_plot_directory = "../plots/v2.5"
-
-    outputs_v2_5_tifs_directory = "../qgis/sample_results/v2.5"
-
-    outputs_v2_5_annual_tifs_directory = "../data/v2.5/annual_sums"
-    outputs_v2_5_annual_plots_directory = os.path.join(
-        model_results_v2_5_plot_directory, "annual_maps"
-    )
-
-    v2_5_mean_climatology_tif = os.path.join(
-        model_results_v2_5_dir, "mean_climatologies/1989_2020_mean_climatology.tif"
-    )
-    v2_5_std_climatology_tif = os.path.join(
-        model_results_v2_5_dir, "mean_climatologies/1989_2020_std_climatology.tif"
-    )
-
-    model_results_dir = model_results_v2_5_dir
-    model_results_picklefile = model_results_v2_5_picklefile
-    model_results_plot_directory = model_results_v2_5_plot_directory
-    output_tifs_directory = outputs_v2_5_tifs_directory
-    outputs_annual_tifs_directory = outputs_v2_5_annual_tifs_directory
-    outputs_annual_plots_directory = outputs_v2_5_annual_plots_directory
-    mean_climatology_geotiff = v2_5_mean_climatology_tif
-    std_climatology_geotiff = v2_5_std_climatology_tif
-    aws_validation_plots_directory = os.path.join(
-        model_results_v2_5_plot_directory, "aws_validation"
-    )
-    gap_fill_data_folder = os.path.join(model_results_v2_5_dir, "gap_fill_data")
-    daily_melt_averages_picklefile = os.path.join(
-        gap_fill_data_folder, "daily_melt_pixel_averages.pickle"
-    )
-    daily_cumulative_melt_averages_picklefile = os.path.join(
-        gap_fill_data_folder, "daily_cumulative_melt_averages.pickle"
-    )
-    gap_filled_melt_picklefile = os.path.join(
-        model_results_v2_5_dir, "v2.5_1979_2021_gap_filled.pickle"
-    )
-    # The percentiles (10,25,50,75,90) saved in a pandas dataframe.
-    baseline_percentiles_csv = os.path.join(
-        model_results_v2_5_dir, "baseline_percentiles_1989-2020.csv"
-    )
-    # Melt data over each region, each day.
-    daily_melt_csv = os.path.join(model_results_v2_5_dir, "daily_melt_totals.csv")
-
-    climatology_plots_directory = os.path.join(
-        model_results_v2_5_plot_directory, "climatology"
-    )
-
-elif DATA_VERSION == 3:
-    model_results_v3_dir = "../data/"
-    model_results_v3_picklefile = os.path.join(
-        model_results_v3_dir, "v3_1979-present_raw.pickle"
-    )
-    model_results_v3_picklefile_gap_filled = os.path.join(
-        model_results_v3_dir, "v3_1979-present_gap_filled.pickle"
-    )
-
-    model_results_dir = os.path.join(model_results_v3_dir, "daily_melt_bin_files")
-    model_results_picklefile = model_results_v3_picklefile
-    model_results_plot_directory = "../plots/"
-    # output_tifs_directory = os.path.join(model_results_v3_dir, "sample_results")
-    outputs_annual_tifs_directory = os.path.join(
-        model_results_v3_dir, "annual_sum_geotifs"
-    )
-    outputs_annual_plots_directory = os.path.join(
-        model_results_plot_directory, "annual_maps_sum"
-    )
-    mean_climatology_geotiff = os.path.join(
-        model_results_v3_dir, "mean_climatology", "1990_2020_mean_climatology.tif"
-    )
-    std_climatology_geotiff = os.path.join(
-        model_results_v3_dir, "mean_climatology", "1990_2020_std_climatology.tif"
-    )
-    gap_fill_data_folder = os.path.join(model_results_v3_dir, "gap_fill_data")
-    daily_melt_averages_picklefile = os.path.join(
-        gap_fill_data_folder, "daily_melt_pixel_averages.pickle"
-    )
-    daily_cumulative_melt_averages_picklefile = os.path.join(
-        gap_fill_data_folder, "daily_cumulative_melt_averages.pickle"
-    )
-    aws_validation_plots_directory = os.path.join(
-        model_results_plot_directory, "aws_validation"
-    )
-    # The percentiles (10,25,50,75,90) saved in a pandas dataframe.
-    baseline_percentiles_csv = os.path.join(
-        model_results_v3_dir, "baseline_percentiles_1990-2020.csv"
-    )
-    daily_melt_csv = os.path.join(model_results_v3_dir, "daily_melt_totals.csv")
-    gap_filled_melt_picklefile = model_results_v3_picklefile_gap_filled
-    climatology_plots_directory = os.path.join(
-        model_results_plot_directory, "annual_line_plots"
-    )
-    threshold_file_dir = os.path.join(model_results_v3_dir, "thresholds")
-    daily_melt_plots_dir = os.path.join(model_results_plot_directory, "daily_maps")
-    daily_plots_gathered_dir = os.path.join(
-        model_results_plot_directory, "daily_plots_gathered"
-    )
+model_results_dir = os.path.join(model_results_v3_dir, "daily_melt_bin_files")
+model_results_picklefile = model_results_v3_picklefile
+model_results_plot_directory = "../plots/"
+# output_tifs_directory = os.path.join(model_results_v3_dir, "sample_results")
+outputs_annual_tifs_directory = os.path.join(
+    model_results_v3_dir, "annual_sum_geotifs"
+)
+outputs_annual_plots_directory = os.path.join(
+    model_results_plot_directory, "annual_maps_sum"
+)
+mean_climatology_geotiff = os.path.join(
+    model_results_v3_dir, "mean_climatology", "1990_2020_mean_climatology.tif"
+)
+std_climatology_geotiff = os.path.join(
+    model_results_v3_dir, "mean_climatology", "1990_2020_std_climatology.tif"
+)
+gap_fill_data_folder = os.path.join(model_results_v3_dir, "gap_fill_data")
+daily_melt_averages_picklefile = os.path.join(
+    gap_fill_data_folder, "daily_melt_pixel_averages.pickle"
+)
+daily_cumulative_melt_averages_picklefile = os.path.join(
+    gap_fill_data_folder, "daily_cumulative_melt_averages.pickle"
+)
+aws_validation_plots_directory = os.path.join(
+    model_results_plot_directory, "aws_validation"
+)
+# The percentiles (10,25,50,75,90) saved in a pandas dataframe.
+baseline_percentiles_csv = os.path.join(
+    model_results_v3_dir, "baseline_percentiles_1990-2020.csv"
+)
+daily_melt_csv = os.path.join(model_results_v3_dir, "daily_melt_totals.csv")
+gap_filled_melt_picklefile = model_results_v3_picklefile_gap_filled
+climatology_plots_directory = os.path.join(
+    model_results_plot_directory, "annual_line_plots"
+)
+threshold_file_dir = os.path.join(model_results_v3_dir, "thresholds")
+daily_melt_plots_dir = os.path.join(model_results_plot_directory, "daily_maps")
+daily_plots_gathered_dir = os.path.join(
+    model_results_plot_directory, "daily_plots_gathered"
+)
